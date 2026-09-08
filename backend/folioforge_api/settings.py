fe9598574120e2436b90228511537da5fa301439
mishrdh43 <mishr.dh43@gmail.com>
2026-09-08 15:30:11 +0530

    Allow all hosts

--- backend/folioforge_api/settings.py
+++ backend/folioforge_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--c%-6vtv&4n7gv#vi&0_)ws8=@%_e=002lp4*4@bub7$&h5i%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
